da6adf861b1c6100937deef2e25ae2f4db7f8786
kolansurgut <73068398+kolansurgut@users.noreply.github.com>
2021-12-19 00:15:56 +0500

    Add files via upload

---
+++ MySQL_config.py
@@ -0,0 +1,53 @@
+import config
+import pymysql
+
+
+class MySQL_config:
+
+    def __init__(self):
+        self.host = config.host
+        self.port = 3306
+        self.user = config.user
+        self.password = config.password
+        self.database = config.database
+        self.connection = self.connect()
+        self.cursor = self.connection.cursor()
+
+    def connect(self):
+        return pymysql.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            database=self.database,
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    def output(self, database):
+        # Выводит базу данных в переменную
+        self.cursor.execute(f"SELECT * FROM {database}")
+        return self.cursor.fetchall()
+
+    def insert(self, database, cells, values):
+        #  "INSERT INTO database (name, age) VALUES ("Anna", 20);"
+        self.cursor.execute(f"INSERT INTO {database} {cells} VALUES {values};")
+        # self.cursor.execute(f"INSERT INTO `{database}` (`id`, `page`) VALUES ('499707202', '1');")
+        self.connection.commit()
+
+    def editing(self, database, command, command2):
+        # "UPDATE database SET password = 'xxxxxx' WHERE id = 'id'"
+        self.cursor.execute(f"UPDATE {database} SET {command} WHERE {command2}")
+        self.connection.commit()
+
+    def del_line(self, database, id):
+        # "DELETE FROM database WHERE id = 'id';"
+        self.cursor.execute(f"DELETE FROM {database} WHERE id = {id};")
+        self.connection.commit()
+
+    def coincidence(self, database, row, msg):
+        self.cursor.execute(f"SELECT * FROM {database} WHERE {row} LIKE '%{msg}%'")
+        teacher_bd = self.cursor.fetchall()
+        if teacher_bd:
+            return teacher_bd
+        else:
+            return False
