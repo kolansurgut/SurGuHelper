f20871efe449b38ca7c337fa5282a4a2435c61ce
kolan <kolansurgut1@mail.ru>
2022-03-23 10:11:49 +0500

    N

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
-token = 'aeeb5f51e0be54d84d32e29ff4a2c5bd85c326051d8ff0403835c30b3723fc1c45fe2c3a740d5b57dfd0a'
-group_id = -207094965
-host = "sitenk03.beget.tech"
-user = "sitenk03_surgu"
-password = "127912mamA"
-database = 'sitenk03_surgu'
-
 list_keyboards = \
     {
         "начать": [False, [['Неделя', 'SECONDARY', False],
